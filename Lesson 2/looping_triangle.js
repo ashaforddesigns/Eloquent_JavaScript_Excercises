7182ed7b9f951c0340a6c43d0c317cf7bab1465f
ashaforddesigns <ashaforddesigns@gmail.com>
2024-12-13 22:10:20 -0600

    first commit

---
+++ Lesson 2/looping_triangle.js
@@ -0,0 +1,8 @@
+// Write a loop that makes seven calls to console.log to output [a certain triangle]
+let character = "#"
+let result = ""
+for (let i = 1; i <= 7; i++){
+    result = result + character;
+console.log(result);
+}
+    
---
+++ README.md
@@ -0,0 +1,4 @@
+# Eloquent JavaScript Excercises
+These are my solutions to the excercises found in the free book [Eloquent JavaScript](https://eloquentjavascript.net)
+## Solutions
+- [Lesson 2](/Lesson%202/)

c3004233146eb243259431a0ccc686f814f21a33
ashaforddesigns <ashaforddesigns@gmail.com>
2024-12-18 16:26:42 -0600

    finished minimum

--- Lesson 3/minimum.js
+++ Lesson 3/minimum.js
@@ -1,1 +1,14 @@
 // Define the function min that takes two arguments and returns their minimum.
+
+
+
+function theMin(a,b) {
+    if (a < b){
+        return a
+    }
+    else{
+        return b
+    }
+        
+}
+console.log(theMin(500,-10))
